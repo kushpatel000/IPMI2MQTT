6b0f37cff0b9a0d41e1b9b79886a041a06d61cd6
kushpatel000 <kushpatel000@gmail.com>
2021-08-21 17:12:04 -0500

    Remove variables
    Removed environment variables for temporary files.

--- ipmi_script.sh
+++ ipmi_script.sh
@@ -1,21 +1,21 @@
 # request data
-ipmitool -H $IPMIHOST -U $IPMIUSER -P $IPMIPASS -I $IPMIVARIABLE1 $IPMIVARIABLE2 > $IPMIFILE
+ipmitool -H $IPMIHOST -U $IPMIUSER -P $IPMIPASS -I $IPMIVARIABLE1 $IPMIVARIABLE2 > ipmi_raw
 
 # iterate through sensors
 for sens_addr in "${SENSOR_ADDR_LIST[@]}"
 do
-	line=$(grep $sens $IPMIFILE)
+	line=$(grep $sens ipmi_raw)
 	name=$( echo $line | cut -d'|' -f1 )
 	status=$( echo $line | cut -d'|' -f 5 | awk '{print $1}' )
 
 	# spoof json file
-	echo "{" > $IPMIDATA
-	echo "    \"name\":      \"$name\" ,"      >> $IPMIDATA
-	echo "    \"unique_id\": \"$sens_addr\" ," >> $IPMIDATA
-	echo "    \"state\":     \"$status\" "     >> $IPMIDATA
-	echo "}" >> $IPMIDATA
+	echo "{" > ipmi_data
+	echo "    \"name\":      \"$name\" ,"      >> ipmi_data
+	echo "    \"unique_id\": \"$sens_addr\" ," >> ipmi_data
+	echo "    \"state\":     \"$status\" "     >> ipmi_data
+	echo "}" >> ipmi_data
 
 	# push data to mosquitto
-	mosquitto_pub -h $MOSQUITTOHOST -u $MOSQUITTOUSER -P $MOSQUITTOPASS -t $MOSQUITTOTOPIC/$sens_addr -f $IPMIDATA
+	mosquitto_pub -h $MOSQUITTOHOST -u $MOSQUITTOUSER -P $MOSQUITTOPASS -t $MOSQUITTOTOPIC/$sens_addr -f ipmi_data
 done
 
