071ad30714c1c43a325bdde8b733faf474ab8904
kushpatel000 <kushpatel000@gmail.com>
2021-08-21 18:55:45 -0500

    Debuggings..

--- ipmi_script.sh
+++ ipmi_script.sh
@@ -1,24 +1,24 @@
 # request data
 ipmitool -H $IPMIHOST -U $IPMIUSER -P $IPMIPASS -I $IPMIVARIABLE1 $IPMIVARIABLE2 > ipmi_raw
 
-# iterate through sensors
-#for sens_addr in "${SENSOR_ADDR_LIST[@]}"
-#do
+echo $SENSOR_ADDR_LIST
+
+# iterate through sensor list
 set -- $SENSOR_ADDR_LIST
-while [ -n $sens_addr ]; do
+for sens_addr in "$@"
+do
 	line=$(grep $sens_addr ipmi_raw)
 	name=$( echo $line | cut -d'|' -f1 )
 	status=$( echo $line | cut -d'|' -f 5 | awk '{print $1}' )
 
-	# spoof json file
+	# create json file
 	echo "{" > ipmi_data
-	echo "    \"name\":      \"$name\" ,"      >> ipmi_data
-	echo "    \"unique_id\": \"$sens_addr\" ," >> ipmi_data
-	echo "    \"state\":     \"$status\" "     >> ipmi_data
+	echo "\"name\":\"$name\","				>> ipmi_data
+	echo "\"unique_id\":\"$sens_addr\","	>> ipmi_data
+	echo "\"state\":\"$status\""			>> ipmi_data
 	echo "}" >> ipmi_data
 
-	# push data to mosquitto
+	# publish MQTT
 	mosquitto_pub -h $MOSQUITTOHOST -u $MOSQUITTOUSER -P $MOSQUITTOPASS -t $MOSQUITTOTOPIC/$sens_addr -f ipmi_data
 	shift
 done
-
